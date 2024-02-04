7d5b7a4fa8f3555503c876a52024e3dc624d5c82
mohjohfox <marcofuchs@mohjohfox.de>
2024-02-04 18:50:21 +0100

    SW-855 Use correct update function for prepared statements

--- src/main/kotlin/de/simpletactics/wiki/lib/adapter/persistence/WikiAdapter.kt
+++ src/main/kotlin/de/simpletactics/wiki/lib/adapter/persistence/WikiAdapter.kt
@@ -14,24 +14,26 @@ class WikiAdapter(
 
     override fun addToWiki(wikiType: WikiType): Int {
         // TODO: Id generation
-        return jdbc.update("INSERT INTO wiki (id, type) VALUES ($0, $wikiType);")
+        return jdbc.update("INSERT INTO wiki (id, type) VALUES (%d,%s);", 0, wikiType)
     }
 
     override fun createTopic(topicEntity: TopicEntity): Int {
-        return jdbc.update("INSERT INTO wiki_topic (id, topic) VALUES (${topicEntity.id}, '${topicEntity.topic}');")
+        return jdbc.update("INSERT INTO wiki_topic (id, topic) VALUES (%d, %s);", topicEntity.id, topicEntity.topic)
     }
 
     override fun updateTopic(topicEntity: TopicEntity): Int {
-        val effectedRows = jdbc.update("UPDATE wiki_topic SET topic = '${topicEntity.topic}' WHERE id = ${topicEntity.id};")
+        val effectedRows = jdbc.update("UPDATE wiki_topic SET topic = %s WHERE id = %d;", topicEntity.topic, topicEntity.id)
         return if (effectedRows == 1) effectedRows else
             throw IllegalStateException("Update topic updated $effectedRows rows instead only 1 for id ${topicEntity.id}. Throw exception for rollback.")
     }
 
     override fun createEntry(entryEntity: EntryEntity): Int {
-        TODO("Not yet implemented")
+        return jdbc.update("INSERT INTO wiki_entry (id, headline, body) VALUES (%d, %s, %s);", entryEntity.id, entryEntity.headline, entryEntity.htmlEntry)
     }
 
     override fun updateEntry(entryEntity: EntryEntity): Int {
-        TODO("Not yet implemented")
+        val effectedRows = jdbc.update("UPDATE wiki_entry SET headline = %s, body = %s WHERE id = %d;", entryEntity.headline, entryEntity.htmlEntry, entryEntity.id)
+        return if (effectedRows == 1) effectedRows else
+            throw IllegalStateException("Update entry updated $effectedRows rows instead only 1 for id ${entryEntity.id}. Throw exception for rollback.")
     }
 }
