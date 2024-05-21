4938759b567657663d990205a87b5b5d22bceed5
Steffen Burgert <sburgert1@stud.hs-offenburg.de>
2024-05-21 15:26:42 +0200

    SW-999: added buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,20 @@ plugins {
 }
 
 apply(plugin = "io.spring.dependency-management")
+apply(plugin = "org.springframework.boot")
+
+buildscript {
+	repositories {
+		maven {
+			url = uri("https://plugins.gradle.org/m2/")
+		}
+	}
+	val springBootVersion: String by project
+	dependencies {
+		classpath("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
+	}
+}
+
 
 repositories {
 	mavenCentral()
