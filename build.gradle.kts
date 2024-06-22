654779cd6de6142950219a2ab14e2496a41810a7
mohjohfox <marcorobinf@gmail.com>
2024-06-22 14:50:50 +0200

    SW-855 build: Set snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,113 +1,124 @@
 group = "de.simpletactics"
-version = "0.0.2"
+version = "0.0.2-SNAPSHOT"
 val javaVersion = "17"
 
 plugins {
-	java
-	`maven-publish`
+    java
+    `maven-publish`
 
-	// Kotlin
-	kotlin("jvm")
-	kotlin("plugin.spring")
-	kotlin("plugin.noarg")
+    // Kotlin
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    kotlin("plugin.noarg")
 
-	id("org.jetbrains.kotlin.plugin.allopen")
-	id("org.springframework.boot")
-	id("com.gorylenko.gradle-git-properties")
-	id("com.github.ben-manes.versions")
+    id("org.jetbrains.kotlin.plugin.allopen")
+    id("org.springframework.boot")
+    id("com.gorylenko.gradle-git-properties")
+    id("com.github.ben-manes.versions")
 
 }
 
 apply(plugin = "io.spring.dependency-management")
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 
-	withSourcesJar()
-	withJavadocJar()
+    withSourcesJar()
+    withJavadocJar()
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	implementation("javax.annotation:javax.annotation-api:1.2-b01")
-	implementation("com.google.code.gson:gson:2.8.9")
-	implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("javax.annotation:javax.annotation-api:1.2-b01")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("org.apache.commons:commons-collections4:4.4")
 
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
 
 tasks {
-	creating(Jar::class) {
-		manifest {
-			attributes["Main-Class"] = "de.simpletactics.wiki.lib.Main.kt"
-		}
-		from(sourceSets.main.get().output)
-		archiveFileName.set("sita-backend.jar")
-	}
+    creating(Jar::class) {
+        manifest {
+            attributes["Main-Class"] = "de.simpletactics.wiki.lib.Main.kt"
+        }
+        from(sourceSets.main.get().output)
+        exclude("**/application-secrets.*")
+        archiveFileName.set("sita-backend.jar")
+    }
 
-	compileKotlin {
-		kotlinOptions {
-			jvmTarget = javaVersion
-		}
-	}
+    compileKotlin {
+        kotlinOptions {
+            jvmTarget = javaVersion
+        }
+    }
 
-	compileTestKotlin {
-		kotlinOptions {
-			jvmTarget = javaVersion
-		}
-	}
+    compileTestKotlin {
+        kotlinOptions {
+            jvmTarget = javaVersion
+        }
+    }
 }
 
 tasks.wrapper {
-	val versionGradle: String by project
-	gradleVersion = versionGradle
+    val versionGradle: String by project
+    gradleVersion = versionGradle
 }
 
 tasks.bootJar {
-	enabled = false
-	mainClass.set("de.simpletactics.Application")
+    enabled = false
+    mainClass.set("de.simpletactics.Application")
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
-	targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.register("bootRunLocal") {
-	group = "application"
-	description = "Runs the Spring Boot application with the local profile"
-	doFirst {
-		tasks.bootRun.configure {
-			systemProperty("spring.profiles.active", "local,secrets")
-		}
-	}
-	finalizedBy("bootRun")
+    group = "application"
+    description = "Runs the Spring Boot application with the local profile"
+    doFirst {
+        tasks.bootRun.configure {
+            systemProperty("spring.profiles.active", "local,secrets")
+        }
+    }
+    finalizedBy("bootRun")
 }
 
 publishing {
-	publications {
-		create<MavenPublication>("maven") {
-			groupId = "de.simpletactics"
-			artifactId = "wiki-lib"
-			version = version
-			from(components["java"])
-		}
-	}
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "de.simpletactics"
+            artifactId = "wiki-lib"
+            version = version
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "nexus"
+            url = uri("https://nexus.mohjohfox.de/nexus/content/repositories/snapshots")
+            credentials {
+                username = "sita"
+                password = "test123"
+            }
+        }
+    }
 }
 
 
