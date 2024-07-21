fdb8181ab1150ff2ab7f8c4fd6f7c727d98b23d4
mohjohfox <marcofuchs@mohjohfox.de>
2024-07-21 13:48:15 +0200

    SW-000 build: Bump java version to 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,11 @@ repositories {
 }
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_11
-	targetCompatibility = JavaVersion.VERSION_11
+	sourceCompatibility = JavaVersion.VERSION_21
+	targetCompatibility = JavaVersion.VERSION_21
 
 	withSourcesJar()
-	withJavadocJar()
+	//withJavadocJar()
 }
 
 repositories {
