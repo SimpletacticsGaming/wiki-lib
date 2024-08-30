a27a67561df846b580d46f008ed733e112f6527c
mohjohfox <marcofuchs@mohjohfox.de>
2024-08-30 17:09:56 +0200

    SW-855 refactoring: Remove unnecessary model

--- src/main/kotlin/de/simpletactics/wiki/lib/model/Right.kt
+++
@@ -1,8 +0,0 @@
-package de.simpletactics.wiki.lib.model
-
-data class Right(
-    val read: Boolean,
-    val edit: Boolean,
-    val create: Boolean,
-    val write: Boolean,
-)
--- src/main/kotlin/de/simpletactics/wiki/lib/model/WikiNavigation.kt
+++
@@ -1,8 +0,0 @@
-package de.simpletactics.wiki.lib.model
-
-data class WikiNavigation(
-    val id: Int,
-    val title: String,
-    val subjectEntries: List<SubjectEntry>,
-    val wikiEntries: List<WikiEntry>,
-)
