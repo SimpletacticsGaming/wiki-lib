64f9aac124c8c45fac06b5ea993be343ce4a7848
Steffen Burgert <sburgert1@stud.hs-offenburg.de>
2024-09-27 15:30:09 +0200

    SW-855: fixed test delete poll

--- src/test/resources/scripts/wiki_poll.sql
+++ src/test/resources/scripts/wiki_poll.sql
@@ -4,7 +4,7 @@ INSERT INTO wiki_poll (id, question, description, end_date, ended, data)
 values (5, 'Frage 1', 'Beschreibung 1', '2023-10-21', 'false',
         '[{"votes": [{"userId": "1", "date": "2023-10-21T00:00", "option": "FALSE"}], "pollOption": {"text": "test1", "uuid": "egal1"}}, {"votes": [{"userId": "2", "date": "2023-10-22T00:00", "option": "TRUE"}], "pollOption": {"text": "test2", "uuid": "egal2"}}]');
 INSERT INTO wiki_topic(id, topic, child_id)
-VALUES (1, 'Thema for poll 2', ARRAY[2]);
+VALUES (1, 'Thema for poll 2', ARRAY [2,6]);
 INSERT INTO wiki_poll (id, question, description, end_date, ended, data)
 values (2, 'This is a test', 'Test description', '2023-10-21', 'false',
         '[{"votes": [], "pollOption": {"text": "testCase", "uuid": "ddb11436-bdc8-4488-87f6-fsdfsd"}}, {"votes": [], "pollOption": {"text": "testCaseTwo", "uuid": "aaa1436-bdc8-4488-87f6-fsdfsd"}}]');
